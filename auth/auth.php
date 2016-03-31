779816194fee00b6e8b26a89f8fba46508100aa2
mhaugan <matshaugan@gmail.com>
2016-03-31 22:05:10 +0200

    commit dis

--- auth/auth.php
+++ auth/auth.php
@@ -1,7 +1,5 @@
 <?php
-    $user = "v16_haugan";
-    $password = "mplx!33t";
-    $dsn = "mysql:dbname=stud_v16_haugan;host=kark.hin.no";
+
 
     try {
         $db = new PDO($dsn, $user, $password);
